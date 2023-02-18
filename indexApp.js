ccc98810a5ba3071ad9d520785612d56c2dfa4e0
Ashley <ashley.emaseko@gmail.com>
2023-02-18 16:41:34 +0200

    Updated the html file with the linked js and css files

---
+++ indexApp.js
@@ -0,0 +1,17 @@
+let h3 = document.querySelector("h3");
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+h3.innerHTML = `${day} ${hours}:${minutes}`;
