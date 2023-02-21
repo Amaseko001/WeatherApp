eeb712dae4c505211b718b6bd61edd8c8469d9f7
Ashley <ashley.emaseko@gmail.com>
2023-02-21 19:06:03 +0200

    edited the html file with form control ID

--- indexApp.js
+++ indexApp.js
@@ -32,3 +32,19 @@ function showWeather(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
 }
+
+function displayWeatherCondition(response) {
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "9cb245c8974a9aa2bee9c6e33954b52a";
+  let city = document.querySelector("#form-control").value;
+  let apiUrl = `https://samples.openweathermap.org/data/2.5/weather?q=${city}appid=${apiKey}&units=metric`;
+}
+axios.get(`apiUrl`).then(`displayWeatherCondition`);
